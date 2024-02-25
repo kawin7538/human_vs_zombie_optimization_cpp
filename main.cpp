3a9a2aef47e1afea6a81ca993cbee7d9a4ad94f5
Kawin Chinpong <kawin7538@hotmail.com>
2024-02-25 16:26:24 +0700

    finalize script and introduce output file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include<fstream>
 #include<vector>
+#include<map>
+#include<string>
 
 #include <pagmo/algorithm.hpp>
 #include <pagmo/algorithms/gwo.hpp>
@@ -14,22 +17,22 @@ using namespace pagmo;
 
 typedef std::vector<int> vector_int;
 
-struct problem_v0 {
+struct human_vs_zombie_problem {
     
     vector_double fitness(const vector_double &dv) const {
 
         vector_int int_dv(dv.begin(),dv.end());
 
-        unsigned short human_stun_remaining = 0;
-        unsigned int human_hp = 100;
-        unsigned int human_ammo = 30;
+        double human_stun_remaining = 0;
+        double human_hp = 100;
+        double human_ammo = 30;
         bool is_human_dead = false;
-        unsigned int human_dead_turn = 1001;
+        double human_dead_turn = 1001;
 
-        unsigned short zombie_stun_remaining = 0;
-        unsigned int zombie_hp = 999;
+        double zombie_stun_remaining = 0;
+        double zombie_hp = 999;
         bool is_zombie_dead =  false;
-        unsigned int zombie_dead_turn = 1001;
+        double zombie_dead_turn = 1001;
 
         int turn_idx = 1;
 
@@ -46,6 +49,7 @@ struct problem_v0 {
                         human_ammo--;
                         if(zombie_hp<=0){
                             is_zombie_dead=true;
+                            zombie_hp=0;
                             zombie_dead_turn=turn_idx;
                             break;
                         }
@@ -63,6 +67,7 @@ struct problem_v0 {
                 // if dv = 4, regenerade human hp
                 else if(dv[turn_idx-1]==4){
                     human_hp+=50;
+                    human_hp=std::min(human_hp,(double)100);
                     human_stun_remaining=1;
                 }
             }
@@ -81,7 +86,7 @@ struct problem_v0 {
             }
         }
 
-        return {zombie_dead_turn+(1001-human_dead_turn)*1000+zombie_hp};
+        return {zombie_dead_turn, zombie_hp, dv[0]-1, -(human_hp-1)};
     }
 
     std::pair<vector_double, vector_double> get_bounds() const {
@@ -94,19 +99,31 @@ struct problem_v0 {
     {
         return 1000;
     }
+
+    vector_double::size_type get_nec() const
+    {
+        return 2;
+    }
+
+    vector_double::size_type get_nic() const
+    {
+        return 1;
+    }
 };
 
 int main()
 
 {
 
+    std::ofstream MyFile("output.txt");
+
     // 1 - Instantiate a pagmo problem constructing it from a UDP
 
-    problem prob{problem_v0()};
+    problem prob{human_vs_zombie_problem()};
 
     // 2 - Instantiate a pagmo algorithm (Improved Harmony Search for 200 iterations)
 
-    algorithm algo{ihs(200u)};
+    algorithm algo{ihs(2000000,0.85,0.35,0.99,1e-5,1,50)};
 
     // 3 - Instantiate an archipelago with 1 islands having each 100 individuals.
 
@@ -126,9 +143,25 @@ int main()
     for (const auto &isl : archi) {
 
         std::cout << isl.get_population() << '\n';
-        std::cout << isl.get_population().champion_f()[0] << '\n';
-        std::cout << isl.get_population().champion_x()[0] << '\n';
 
+        if((isl.get_population().champion_f()[1]==0)&&(isl.get_population().champion_f()[3]<0)){
+            std::map<double, std::string> dv_to_str_solution;
+            dv_to_str_solution[1]="Shoot That ZOMBIE";
+            dv_to_str_solution[2]="Reload AK-47";
+            dv_to_str_solution[3]="Flash Grenade";
+            dv_to_str_solution[4]="Regenerated HP";
+
+            MyFile << "Steps to kill that zombies" << '\n';
+            int step_idx = 1;
+            for (const auto &x : isl.get_population().champion_x()){
+                MyFile << step_idx <<". " << dv_to_str_solution[x] << '\n';
+                step_idx+=1;
+                if(step_idx>isl.get_population().champion_f()[0]){
+                    break;
+                }
+            }
+            MyFile << "ZOMBIE WENT TO HELL!!!!" << '\n';
+        }
     }
 
     return 0;
